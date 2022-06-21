3616b0ff258c7c0af6c8681f211325943ede96ef
Simon Palma <simonpc55@gmail.com>
2022-06-21 16:54:34 +0200

    test deploy

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+docker build -t simonpcpc/multi-client:latest -t simonpcpc/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t simonpcpc/multi-server:latest -t simonpcpc/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t simonpcpc/multi-worker:latest -t simonpcpc/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push simonpcpc/multi-client:latest
+docker push simonpcpc/multi-server:latest
+docker push simonpcpc/multi-worker:latest
+
+docker push simonpcpc/multi-client:$SHA
+docker push simonpcpc/multi-server:$SHA
+docker push simonpcpc/multi-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/client-deployment client=simonpcpc/multi-client:$SHA
+kubectl set image deployments/server-deployment server=simonpcpc/multi-server:$SHA
+kubectl set image deployments/worker-deployment worker=simonpcpc/multi-worker:$SHA
